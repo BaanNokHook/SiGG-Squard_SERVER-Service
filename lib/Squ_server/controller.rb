11928caab7f9f277906b471f162739bee4dcbd14
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-13 16:12:15 +0700

    SiGG-Squard_SERVER-Service-lib-Squ_server-controller.rb

---
+++ lib/Squ_server/controller.rb
@@ -0,0 +1,56 @@
+require_relative 'player'  
+require_relative 'session'   
+
+module Squ_server  
+   class Controller  
+
+      # @param client [TCPSocket]  
+      def initialize(client)     
+         @socket = client  
+         @session = Session.instance 
+      end  
+
+      # @param user [String]  
+      def connexion(user)  
+        if Player.exist? user 
+           @socket.puts 'REFUS/'   
+        else  
+           @session.synchronize { # la session est un singleton, on peut donc se synchroniser dessu 
+             @player = Player.new(@socket, user)   
+             @session.start if @player.alone?              
+           }
+        @player.puts "BIENVENUE/#{@session.grid}/#{@session.tour.tirage.join}/#{Player.scores}/#{@session.phase}/#{@session.time}/"
+        @player.broadcast "CONNECTE/#{user}/"
+      end
+      @player
+    end
+
+    # @param user [String]
+    def sort(user)
+      if @player.name == user
+        @player.destroy
+      end
+    end
+
+    # @param placement [String]
+    def trouve(placement)
+      @session.trouve(placement, @player) if  @session.synchronize { @session.phase == :REC || @session.phase == :SOU }
+    end
+
+    # @param msg [String]
+    def envoi(msg)
+      @player.broadcast "RECEPTION/#{msg}/"
+    end
+
+    # @param user [String]
+    # @param msg [String]
+    def penvoi(user, msg)
+      dst = Player.find(user)
+      src = @player.name
+      if dst
+        dst.puts "PRECEPTION/#{msg}/#{src}/"
+      end
+    end
+
+  end
+end
